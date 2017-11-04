07c34721a1deef8bf3990b365a7aa3f1911dee8c
Payotz <payotz@localhost.localdomain>
2017-11-04 11:59:19 -0700

    Set CLI to default

--- script/default.js
+++ script/default.js
@@ -1,15 +1,15 @@
 var interface = 0;
 
 function enableGUI(){
-    document.getElementById('center').style.display = 'none';
     document.getElementById('gui').style.display = 'block';
     interface = 1;
 }
 
 function enableCLI(){
-    document.getElementById('center').style.display = 'none';
     document.getElementById('cli').style.display = 'block';
     interface = 2;
     limit = document.getElementById('prompt_text').innerHTML.length;
     test()
 }
+
+window.onload = enableCLI
